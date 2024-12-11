be61e7ac25adee6ec56c34be846e0fa85d0aa770
amithbhat1 <amithbhat01@gmail.com>
2024-12-10 18:45:54 -0600

    improve test.cpp

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,7 @@ int main (int argc, char * argv [])
 	int num_records = 10000;
 
     cout << "Program usage:\nUser may provide one arguments: number of records(to be sorted) [with -n option]\n";
-    cout << "Program Behaviours:\n1.If you have not specified either or any of the required arguments, we assume default values of:\n \tNumber of records: " << num_records << endl;
+    cout << "Program Behaviours:\n1.If you have not specified either or any of the required arguments, we assume default values of:\tNumber of records: " << num_records << endl;
     cout << "2.The number of records must be between 1 and 10^8.\n";
 
     if(argc < 2){
@@ -26,6 +26,8 @@ int main (int argc, char * argv [])
                         cout<<"We cannot have negative number of records in the database ----- Exiting the program\n";
                     } else if(num_records == 0){
                         cout<<"Number of records entered = 0.\n There are no records in the database ----- Exiting the program\n";
+                    } else{
+                        cout<<"*********Number of records: "<<num_records<<endl;
                     }
                 } catch (const exception& e) {
                     num_records = 10000;
